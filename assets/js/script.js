f1b1369001a6da7f84765213e0122453353ccfc8
Reynaldo Candido <reynaldo.candido.ads@gmail.com>
2021-06-13 19:29:14 -0300

    Finalização do modal Ver Mais

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,19 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+function closeModal() {
+    var modal = document.querySelector('.modal');
+    var modalContainer = document.querySelector('.modal-container')
+    modal.style.display = "none"
+    modalContainer.style.display = "none";
+}
+
+function openModal() {
+    var modalContainer = document.querySelector('.modal-container')
+    var modal = document.querySelector('.modal')
+    modal.style.display = "flex";
+    modalContainer.style.display = "flex";
+    modal.classList.add('modalAnimation')
+}
+
